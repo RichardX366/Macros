33eda215bb78197ab609d190a00fdb26359159c6
Richard Xiong <richardx366@gmail.com>
2026-06-11 16:00:36 -0400

    winrate mute volume

--- limbus/win_rate.py
+++ limbus/win_rate.py
@@ -1,10 +1,28 @@
 from time import sleep
+from typing import Any
 
 from pyautogui import press
 from helpers import WindowHelper
+import keyboard
+from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
+
+sessions = AudioUtilities.GetAllSessions()
+volume: Any = None
+
+for session in sessions:
+    if session.Process:
+        if session.Process.name() == "LimbusCompany.exe":
+            volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+
 
 if __name__ == "__main__":
+    keyboard.add_hotkey(
+        "ctrl+\\", lambda: volume.SetMute(not volume.GetMute(), None), suppress=True
+    )
+
     wh = WindowHelper("LimbusCompany", 1.5)
+
+    print("Win rate ready. Press Ctrl + \\ to toggle volume.")
 
     while True:
         if wh.click_text("Win", top=0.7, left=0.7, height=0.2):
